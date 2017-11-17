5731c33349cfc16d02ca06d48b1cba1ab5a729e2
Tim Sharp <timothyaaron.sharp@gmail.com>
2017-11-17 15:54:18 -0700

    commit

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose')
+var ObjectId = Schema.Types.ObjectId
 
 var schema = new mongoose.Schema({
   body: {type: String, required: true},
   votes: {type: Number, default: 0},
   health: {type: Number, default: 100},
   alive: {type: Boolean, default: true},
+  creatorId: {type: ObjectId, ref: 'User'},
+  postId: {type: ObjectId, ref: 'Post'},
   created: {type: Number, defualt: Date.now()}  
 })
 
