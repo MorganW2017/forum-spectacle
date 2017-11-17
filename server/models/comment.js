6ba7be4002f9dceb4de7d49dacf691e02624e1b7
Morgan Waters <morangwaters@gmail.com>
2017-11-17 14:28:16 -0700

    alive!

--- server/models/comment.js
+++ server/models/comment.js
@@ -4,6 +4,7 @@ var schema = new mongoose.Schema({
   body: {type: String, required: true},
   votes: {type: Number, default: 0},
   health: {type: Number, default: 100},
+  alive: {type: Boolean, default: true},
   created: {type: Number, defualt: Date.now()}  
 })
 
