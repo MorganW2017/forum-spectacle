cc4c80c2586178213876736048fb996eafc6b7e4
Ian Bradley <ianbradleydebate@gmail.com>
2017-11-17 18:37:38 -0700

    is this working

--- public/app/init.js
+++ public/app/init.js
@@ -2,13 +2,8 @@ var app = {
     controllers:{
         postController: new PostController(),
         loginController: new LoginController(),
-<<<<<<< HEAD
         // userController: new UserController()
         // commentController: new CommentController(),
-=======
-        userController: new UserController(),
-        commentController: new CommentController()
->>>>>>> 4599277bee86f358a0b3124581b7435ceabd99c1
         // signupController: new SignupController(),
     }
 }
