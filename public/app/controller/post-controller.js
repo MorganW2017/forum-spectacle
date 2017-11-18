92ebe9febe0f445e07f71cd76c2d8b74655406f9
Ian Bradley <ianbradleydebate@gmail.com>
2017-11-18 01:49:43 -0700

    more css changes

--- public/app/controller/post-controller.js
+++ public/app/controller/post-controller.js
@@ -1,40 +1,58 @@
-function PostController(){
+function PostController() {
     var postService = new PostService()
-    
-    function getPosts(){
+
+    var firstSentence = ''
+
+    function getPosts() {
         postService.getPosts(drawPosts)
     }
-    function drawPosts(posts){
+    function drawPosts(posts) {
         var postElem = document.getElementById('postId')
         var template = ''
         for (let i = 0; i < posts.length; i++) {
             const post = posts[i];
+            console.log(`${specialSentence(post.description)}`)
             template += `
             <div class="panel panel-default text-center spacer">
-            <h3>${post.title}</h3>
-            <h1>${post.description}</h1>
-            <div class="comment-tracker">
-                <h4 style="display:inline">Alive: 12</h4>
-                <h4 style="display:inline">Dead: 3</h4>
-                <button onclick="app.controllers.commentController.getComments('${post._id}')">Comments</button>
-                <i class=" fa fa-trash ilb pull-right" onclick="app.controllers.postController.deletePost('${post._id}')"></i>
+                <div class="panel-heading">
+                    <h1>${post.title}</h1>
+                </div>
+                <div class="panel-body">
+                    <h1 class="first">${sentArr[0]}</h1>
+                    <h3 id="Second">${sentArr[1]}</h3>
+                </div>
+                <div class="panel-footer">
+                    <div class="comment-tracker">
+                        <button onclick="app.controllers.commentController.getComments('${post._id}')">Comments</button>
+                        <i class=" fa fa-trash ilb pull-right" onclick="app.controllers.postController.deletePost('${post._id}')"></i>
+                    </div>
+                </div>
+                    <div id="${post._id}"></div>
             </div>
-            <div id="${post._id}"><div>
-        </div>
             `
-            
+
         }
         postElem.innerHTML = template
     }
-    this.addPost = function addPost(event){
+
+    function specialSentence(str) {
+        var strArr = str.split('. ')
+        firstSentence = strArr[0] + "."
+        var afterFirst = strArr.splice(1, strArr.length)
+        var second = afterFirst.join('. ')
+        return sentArr = [firstSentence, second]
+        console.log(firstSentence)
+        console.log(second)
+    }
+    this.addPost = function addPost(event) {
         event.preventDefault()
         var post = event.target
-            postService.addPost(post, getPosts)
+        postService.addPost(post, getPosts)
     }
-    this.deletePost = function deletePost(index){
+    this.deletePost = function deletePost(index) {
         postService.deletePost(index, getPosts)
     }
-    this.searchPosts = function searchPosts(event){
+    this.searchPosts = function searchPosts(event) {
         event.preventDefault()
         var search = event.target.search.value
         postService.searchPosts(search, drawPosts)
